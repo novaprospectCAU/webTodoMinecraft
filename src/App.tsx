4e921c2a56e7c229312afacfbf09f872bf2b901d
novaprospectCAU <novaprospect110a@gmail.com>
2024-09-04 08:30:35 +0900

    기본 프로젝트 세팅을 하였습니다. 그래픽 라이브러리는 아직 추가하지 않았습니다.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div className="background">
+        <canvas></canvas>
+      </div>
+    </>
+  );
+};
+
+export default App;
